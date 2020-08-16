17892994a63da2ea9ee3f94ea17cb07540e3bc3f
dis0ps <dis0ps.email@gmail.com>
2020-08-16 13:52:54 -0400

    more refactoring and adds view types

--- javascript/library/js/library.js
+++ javascript/library/js/library.js
@@ -1,16 +1,34 @@
 const libraryDiv = document.querySelector(".library");
 
+//TODO: firebase login via google or other external auth
+//TODO: Toggle flip of currentView and render accordingly
+//TODO: firebase storage by JSON.stringify of book objects
+//TODO: Modal dialog for adding new book using book constructor
+
 class Book {
   constructor(title, author, pages, read = false) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.id = this.generateId(`${this.title}${this.author}`.replace(" ", ""));
+  }
+
+  generateId(inputString) {
+    //https://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript
+    let hash = 0;
+    for (let i = 0; i < inputString.length; i++) {
+      let chr = inputString.charCodeAt(i);
+      hash = (hash << 5) - hash + chr;
+      hash |= 0; // Convert to 32bit integer
+    }
+    return hash.toString();
   }
 
   render(cssClass) {
     let article = document.createElement("article");
     article.classList.add(cssClass);
+    article.dataset.id = this.id;
 
     let articleTitle = document.createElement("header");
     articleTitle.innerHTML = `<span>${this.title}</span>`;
@@ -38,10 +56,11 @@ class Book {
 }
 
 class Library {
-  constructor(targetDiv) {
+  constructor(targetDiv, bookClass) {
     this.bookShelf = [];
-    this.currentView = [];
     this.targetDiv = targetDiv;
+    this.bookClass = bookClass;
+    this.currentView = "all";
     this.addHandling(this.targetDiv);
   }
 
@@ -49,25 +68,16 @@ class Library {
     targetDiv.addEventListener("click", (e) => {
       //Identify click event source and book
       let clickedBook = e.target.closest(".book");
+      let bookId = clickedBook.dataset.id;
+
       let clickedAction = e.target.getAttribute("id");
 
-      //Match book clicked to the array
-      let libraryArray = Array.from(targetDiv.querySelectorAll(".book"));
-      let bookIndex = libraryArray.indexOf(clickedBook);
-
       if (clickedAction == "delete") {
-        //Remove affected book from array and DOM
-        this.bookShelf.splice(bookIndex, 1);
-        targetDiv.removeChild(clickedBook);
+        this.removeBook(bookId);
       }
 
-      //Replace affected book from array and DOM
       if (clickedAction == "bookmark") {
-        this.bookShelf[bookIndex].read = !this.bookShelf[bookIndex].read;
-        targetDiv.replaceChild(
-          this.bookShelf[bookIndex].render("book"),
-          clickedBook
-        );
+        this.toggleBookmark(bookId);
       }
     });
   }
@@ -76,26 +86,49 @@ class Library {
     this.bookShelf.push(newBook);
   }
 
-  renderAll(cssClass) {
+  removeBook(bookId) {
+    let filteredBooks = this.bookShelf.filter((book) => book.id != bookId);
+    this.bookShelf = filteredBooks;
+    this.renderView();
+  }
+
+  toggleBookmark(bookId) {
+    let bookIndex = this.bookShelf.findIndex((book) => book.id == bookId);
+    console.log(bookIndex);
+    if (bookIndex != -1) {
+      this.bookShelf[bookIndex].read = !this.bookShelf[bookIndex].read;
+      this.renderView();
+    }
+  }
+
+  renderView() {
+    //Clear div
     this.targetDiv.innerText = "";
-    this.bookShelf.forEach((currentBook) => {
-      this.targetDiv.appendChild(currentBook.render(cssClass));
-      this.targetDiv.lastChild.dataset.array-index=
+    let viewFunction = (book) => {
+      return true;
+    };
+
+    switch (this.currentView) {
+      case "read":
+        viewFunction = (book) => book.read == true;
+        break;
+      case "unread":
+        viewFunction = (book) => book.read == false;
+        break;
+    }
+    let s = (book) => (book = true);
+
+    this.bookShelf.filter(viewFunction).forEach((currentBook) => {
+      this.targetDiv.appendChild(currentBook.render(this.bookClass));
     });
   }
 }
 
-let myLibrary = new Library(libraryDiv);
+let myLibrary = new Library(libraryDiv, "book");
 
 myLibrary.addBook(new Book("Cryptonomicon", "Neal Stephenson", 918, true));
 myLibrary.addBook(new Book("The Cuckoo's Egg", "Clifford Stoll", 326, false));
-myLibrary.addBook(new Book("Cryptonomicon", "Neal Stephenson", 918, true));
-myLibrary.addBook(new Book("Cryptonomicon", "Neal Stephenson", 918, true));
-myLibrary.addBook(new Book("Cryptonomicon", "Neal Stephenson", 918, true));
-myLibrary.addBook(new Book("Cryptonomicon", "Neal Stephenson", 918, false));
-myLibrary.addBook(new Book("The Cuckoo's Egg", "Clifford Stoll", 326, true));
-myLibrary.addBook(new Book("The Cuckoo's Egg", "Clifford Stoll", 326, true));
-myLibrary.addBook(new Book("The Cuckoo's Egg", "Clifford Stoll", 326, false));
-myLibrary.addBook(new Book("The Cuckoo's Egg", "Clifford Stoll", 326, true));
+myLibrary.addBook(new Book("The Dip", "Some Guy", 70, true));
 
-myLibrary.renderAll("book");
+myLibrary.currentView = "all";
+myLibrary.renderView();
