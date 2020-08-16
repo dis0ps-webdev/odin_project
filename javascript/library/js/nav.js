f80c6c897160aaf40cbff2ea56ee72cca0db529b
David V Rodriguez <david@Davids-MacBook-Pro.local>
2020-08-16 18:12:31 -0400

    adds toggle for filter view

--- javascript/library/js/nav.js
+++ javascript/library/js/nav.js
@@ -1,5 +1,8 @@
 const navElement = document.querySelector("nav#main");
+const spanCurrentFilter = document.querySelector("#current-filter");
 const navOriginalPosition = navElement.offsetTop;
+const filterOptions = ["all", "read", "unread"];
+let currentFilterIndex = 0;
 
 function makeSticky() {
   if (navElement.offsetTop < window.pageYOffset) {
@@ -9,5 +12,28 @@ function makeSticky() {
     navElement.classList.remove("sticky");
   }
 }
+function updateFilterOption() {
+  spanCurrentFilter.textContent = filterOptions[currentFilterIndex];
+  if (currentFilterIndex == 1) {
+    spanCurrentFilter.classList.add("red");
+  } else {
+    spanCurrentFilter.classList.remove("red");
+  }
+}
+
+function handleClick(e) {
+  let clickedItem = e.target.id;
+
+  if (e.target.id == "filter") {
+    if (currentFilterIndex == 2) {
+      currentFilterIndex = 0;
+    } else {
+      currentFilterIndex += 1;
+    }
+    updateFilterOption();
+  }
+}
 
 window.addEventListener("scroll", makeSticky);
+navElement.addEventListener("click", handleClick);
+updateFilterOption();
