9f5bf457a2aa5f1b0cbcdded2766c363ae73d422
dis0ps <dis0ps.email@gmail.com>
2021-02-13 17:52:10 -0500

    aligns pubsub and creates constants for general app

--- javascript/todos/src/app/config/AppConfig.ts
+++ javascript/todos/src/app/config/AppConfig.ts
@@ -1,4 +1,6 @@
 import elementClosest from "element-closest";
+import { compareAsc, format } from "date-fns";
+import { Project } from "../model/Project";
 
 //Element.closest and Element.matches polyfills
 elementClosest(window);
@@ -6,3 +8,14 @@ elementClosest(window);
 export const containerDiv: Element = <Element>(
   document.querySelector("#container")
 );
+
+export enum enumPriorities {
+  LOW = 1,
+  MEDIUM = 2,
+  HIGH = 3
+}
+
+export enum enumEventMessages {
+  UPDATED_PROJECT = 1
+
+}
--- javascript/todos/src/pages/index.ts
+++ javascript/todos/src/pages/index.ts
@@ -15,7 +15,43 @@ const header = new components.Header(config.containerDiv, pubsub);
 const footer = new components.Footer(config.containerDiv, pubsub);
 const todoList = new components.TodoList(config.containerDiv, pubsub);
 
+const testProject = new Project();
+const td1 = new TodoData();
+
+td1.name = "Todo 1";
+td1.description = "Todo Description";
+td1.dueDate = new Date();
+
+const objTodo1 = new Todo(td1);
+
+const td2 = new TodoData();
+
+td2.name = "Todo 2";
+td2.description = "Todo Description";
+td2.dueDate = new Date();
+
+const objTodo2 = new Todo(td2);
+
+const td3 = new TodoData();
+
+td3.name = "fiona";
+td3.description = "uuu45uu6uu8rrfuguuuurg6errh";
+td3.dueDate = new Date();
+
+const objTodo3 = new Todo(td3);
+
 header.render();
 footer.render();
 
 todoList.render();
+
+testProject.addTodoItem(objTodo1);
+pubsub.publish(config.enumEventMessages.UPDATED_PROJECT, testProject);
+
+testProject.addTodoItem(objTodo2);
+pubsub.publish(config.enumEventMessages.UPDATED_PROJECT, testProject);
+
+const testTimeout = setTimeout(() => {
+  testProject.addTodoItem(objTodo3);
+  pubsub.publish(config.enumEventMessages.UPDATED_PROJECT, testProject);
+}, 5000);
