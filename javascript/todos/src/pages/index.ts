fea8babb3a6b9da78be609ad7818908f63af5b57
dis0ps <dis0ps.email@gmail.com>
2021-02-15 18:01:32 -0500

    incorporates App summary export usage and Factory object

--- javascript/todos/src/pages/index.ts
+++ javascript/todos/src/pages/index.ts
@@ -1,19 +1,42 @@
 import * as layout from "../css/layout.global.scss";
 import * as typography from "../css/typography.global.scss";
-import * as config from "../app/config/AppConfig";
 import * as components from "../components/Components";
-import { PubSub } from "../app/pubsub/PubSub";
+import * as app from "../app/App";
 
 const global_layout = layout;
 const global_typography = typography;
 
-const pubsub = new PubSub();
+const pubsub = new app.PubSub();
 
-const header = new components.Header(config.containerDiv, pubsub);
-const footer = new components.Footer(config.containerDiv, pubsub);
-const todoEdit = new components.TodoEdit(config.containerDiv, pubsub);
+const header = new components.Header(app.containerDiv, pubsub);
+const footer = new components.Footer(app.containerDiv, pubsub);
+const todoEdit = new components.TodoEdit(app.containerDiv, pubsub);
+const todoList = new components.TodoList(app.containerDiv, pubsub);
 
 header.render();
 footer.render();
+todoList.render();
 
-todoEdit.render();
+let project1 = app.Factory.createProjectTemplate();
+let thisProject = app.Factory.createProject(project1);
+
+let todo1 = app.Factory.createTodoTemplate();
+todo1.name = "Todo 1";
+todo1.description = "Todo Description";
+todo1.dueDate = new Date();
+
+thisProject.addTodoItem(todo1);
+
+pubsub.publish(app.enumEventMessages.UPDATED_PROJECT, thisProject);
+
+setTimeout(() => {
+  let tempTodo = thisProject.getTodoItem(todo1.id);
+  if (tempTodo) {
+    let tempData = tempTodo.getData();
+    tempData.description = "This was updated";
+    tempData.priority = app.enumPriorities.High;
+    tempTodo.updateData(tempData);
+  }
+
+  pubsub.publish(app.enumEventMessages.UPDATED_PROJECT, thisProject);
+}, 5000);
