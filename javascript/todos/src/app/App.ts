84bf909ad0f8b18003b02f65a1430556be40c80c
dis0ps <dis0ps.email@gmail.com>
2021-02-15 18:47:11 -0500

    stubs out controller app object

--- javascript/todos/src/app/App.ts
+++ javascript/todos/src/app/App.ts
@@ -1,3 +1,5 @@
 export * from "./config/AppConfig";
 export * from "./factory/Factory";
 export * from "./pubsub/PubSub";
+export * from "./controller/Controller";
+export * from "./router/PageRouter";
