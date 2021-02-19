0a4d3ee8a64cd408daf22964b0a676909945f663
dis0ps <dis0ps.email@gmail.com>
2021-02-18 22:45:59 -0500

    adds updating of total count via pubsub messages

--- javascript/todos/src/components/Footer/Footer.ts
+++ javascript/todos/src/components/Footer/Footer.ts
@@ -4,12 +4,24 @@ import * as app from "../../app/App";
 
 class Footer extends Component {
   private refPubSub: app.PubSub;
+  private currentProject: app.Project;
+  private numTodos: number = 0;
 
   constructor(container: Element, pubsub: app.PubSub) {
     super(container);
     this.refPubSub = pubsub;
+    this.refPubSub.subscribe(
+      app.enumEventMessages.UPDATE_VIEWS,
+      this.handleUpdateView.bind(this)
+    );
     this._bindHandler("click", this.handleClick.bind(this));
     this._bindHandler("change", this.handleChange.bind(this));
+  }
+
+  private handleUpdateView(data: app.Project) {
+    this.currentProject = data;
+    this.numTodos = data.getData().arrTodo.length;
+    this._updateOutputElement();
   }
 
   private handleClick(e: Event) {
@@ -20,6 +32,11 @@ class Footer extends Component {
         case "add-button":
           this.refPubSub.publish(app.enumEventMessages.CHANGE_VIEW_EDIT, null);
           break;
+        case "settings-button":
+          this.refPubSub.publish(
+            app.enumEventMessages.CHANGE_VIEW_SETTINGS,
+            null
+          );
       }
     }
   }
@@ -27,13 +44,17 @@ class Footer extends Component {
   private handleChange(e: Event) {
     const targetSelect = e.target as HTMLInputElement;
     console.log(`Change: ${targetSelect.value}`);
+    this.refPubSub.publish(
+      app.enumEventMessages.CHANGE_PROJECT,
+      targetSelect.value
+    );
   }
 
   protected _updateOutputElement() {
     this.outputElement.className = styles["footer"];
     this.outputElement.innerHTML = `
     <div class=${styles["top-footer"]}>
-      <span>Todo Items: 3</span>
+      <span>Total Todos: ${this.numTodos}</span>
       <img id="add-button" src="images/add_item_icon.svg" alt="Add Item" srcset="" />
     </div>
 
