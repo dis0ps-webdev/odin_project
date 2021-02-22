a2682d8a6c2d4e95c2a83bf07ac32742a077cc32
dis0ps <dis0ps.email@gmail.com>
2021-02-21 21:44:22 -0500

    wires up handle of project add/delete

--- javascript/todos/src/components/Settings/Settings.ts
+++ javascript/todos/src/components/Settings/Settings.ts
@@ -11,6 +11,18 @@ class Settings extends Component {
     this.refPubSub = pubsub;
 
     this._bindHandler("click", this.handleClick.bind(this));
+    this.refPubSub.subscribe(
+      app.enumEventMessages.UPDATE_VIEWS,
+      this.handleUpdateView.bind(this)
+    );
+  }
+
+  private handleUpdateView(data: app.ProjectList) {
+    const loadedProject = data.getCurrentProject();
+    if (loadedProject) {
+      console.log(loadedProject);
+      this.currentProject = loadedProject;
+    }
   }
 
   private handleClick(e: Event) {
@@ -18,12 +30,55 @@ class Settings extends Component {
       const targetClick = e.target as HTMLElement;
       switch (targetClick.id) {
         case "add-button":
+          this.handleAddProject();
           break;
         case "delete-button":
+          this.handleDeleteProject();
           break;
         case "cancel-button":
           this.refPubSub.publish(app.enumEventMessages.CHANGE_VIEW_LIST, null);
           break;
+      }
+    }
+  }
+
+  private handleAddProject() {
+    let projectTemplate = app.Factory.createProjectTemplate();
+    let titleElement = <HTMLInputElement>(
+      this.targetContainer.querySelector("#title")
+    );
+    if (titleElement) {
+      if (titleElement.value !== "") {
+        projectTemplate.name = titleElement.value;
+        this.refPubSub.publish(
+          app.enumEventMessages.ADD_PROJECT,
+          projectTemplate
+        );
+        this.refPubSub.publish(
+          app.enumEventMessages.CHANGE_PROJECT,
+          projectTemplate.id
+        );
+        this.refPubSub.publish(app.enumEventMessages.CHANGE_VIEW_LIST, null);
+      }
+    }
+  }
+  private handleDeleteProject() {
+    const titleElement = <HTMLInputElement>(
+      this.targetContainer.querySelector("#confirm-delete")
+    );
+
+    const currentProjectData = this.currentProject.getData();
+
+    if (titleElement) {
+      if (
+        titleElement.value == "DELETE" &&
+        currentProjectData.name != "Default"
+      ) {
+        this.refPubSub.publish(
+          app.enumEventMessages.DELETE_PROJECT,
+          currentProjectData.id
+        );
+        this.refPubSub.publish(app.enumEventMessages.CHANGE_VIEW_LIST, null);
       }
     }
   }
@@ -38,7 +93,7 @@ class Settings extends Component {
       <button type="button" id="add-button">Add Project</button>
 
       <label for="title">Remove Project</label>
-      <input type="text" name="confirm-delete" id="confirm-delete" placeholder="Type DELETE to confirm"/>
+      <input type="text" name="confirm-delete" id="confirm-delete" placeholder="Type DELETE to confirm, cannot delete Default project"/>
       <button type="button" id="delete-button">Delete Current</button>
 
       <button type="button" id="cancel-button">Cancel</button>
