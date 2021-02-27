c39d5d9a27cb4437b39d461aff7b1835535e1009
dis0ps <dis0ps.email@gmail.com>
2021-02-27 13:51:24 -0500

    adds HTML validity checks using Validity API

--- javascript/library/js/book-modal.js
+++ javascript/library/js/book-modal.js
@@ -3,6 +3,12 @@ const addBookModal = document.querySelector(".modal.add-book");
 const btnCancelBook = document.querySelector("#book-cancel");
 const btnSubmitBook = document.querySelector("#book-submit");
 const bookFieldElements = document.querySelectorAll("input");
+const bookAdditionForm = document.querySelector("#form-add-book");
+
+const errorMessages = {
+  badInput: "Enter a valid number of pages",
+  valueMissing: "This field is required",
+};
 
 function openAddBookModal() {
   addBookModal.style.display = "block";
@@ -10,19 +16,63 @@ function openAddBookModal() {
 
 function closeBookModal() {
   addBookModal.style.display = "none";
-  bookFieldElements.forEach((field) => (field.value = ""));
+  bookFieldElements.forEach((field) => {
+    clearErrorMessage(field);
+    field.value = "";
+  });
 }
 
 function submitBook() {
-  //myLibrary.addBook(new Book(bookFieldElements[0].value));
   let newBook = new Book(
     ...Array.from(bookFieldElements).map((field) => field.value)
   );
-  myLibrary.addBook(newBook);
-  myLibrary.renderView();
-  closeBookModal();
+
+  const isValid = isValidForm();
+
+  if (isValid) {
+    myLibrary.addBook(newBook);
+    myLibrary.renderView();
+    closeBookModal();
+  }
 }
 
-btnCancelBook.addEventListener("click", closeBookModal);
-btnSubmitBook.addEventListener("click", submitBook);
+function setErrorMessage(field) {
+  const invalidityState = field.validity;
+  const errorElement = document.getElementById(`${field.name}-error`);
+
+  for (validityType in invalidityState) {
+    if (invalidityState[validityType] == true) {
+      errorElement.innerHTML = errorMessages[validityType];
+      errorElement.classList.add("visibile");
+    }
+  }
+}
+
+function clearErrorMessage(field) {
+  const errorElement = document.getElementById(`${field.name}-error`);
+  errorElement.classList.remove("visibile");
+}
+
+function isValidForm() {
+  return Array.from(bookFieldElements).every((field) => {
+    const valid = field.checkValidity();
+
+    if (!valid) {
+      setErrorMessage(field);
+      return false;
+    } else {
+      clearErrorMessage(field);
+      return true;
+    }
+  });
+}
+
+btnCancelBook.addEventListener("click", (e) => {
+  e.preventDefault();
+  closeBookModal();
+});
+btnSubmitBook.addEventListener("click", (e) => {
+  e.preventDefault();
+  submitBook();
+});
 addBookButton.addEventListener("click", openAddBookModal);
