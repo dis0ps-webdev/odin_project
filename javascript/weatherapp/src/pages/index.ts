72cb0d8871837e462aeb444da1a6ab3fe9e0abc5
dis0ps <dis0ps.email@gmail.com>
2021-04-25 14:01:10 -0400

    testing

--- javascript/weatherapp/src/pages/index.ts
+++ javascript/weatherapp/src/pages/index.ts
@@ -1,8 +1,27 @@
 import * as layout from "../css/layout.global.scss";
 import * as typography from "../css/typography.global.scss";
-import * as config from "../app/config/AppConfig";
+import * as app from "../app/App";
+import { WeatherService } from "../app/App";
 
 const global_layout = layout;
 const global_typography = typography;
 
-config.containerDiv.innerHTML = "<div>Hello World!</div>";
+app.containerDiv.innerHTML = "<div>Hello World!</div>";
+const weather = new app.WeatherService();
+weatherProcess().then((data) => console.log(data));
+
+//IIFE, maybe cleaner with a than()
+/*
+(async () => {
+  let testData = await weather.getLocation("springfield");
+  console.log(testData);
+})();
+*/
+async function weatherProcess() {
+  let location = await weather.getLocation("Springfield");
+  console.log(location)
+  weather.setLocation(location[1]);
+  let weatherData = weather.getWeather();
+  return weatherData;
+
+}
