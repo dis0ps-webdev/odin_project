0c373c07ec2fb597c2c2cdb5ea3c4bdfe37f56fd
dis0ps <dis0ps.email@gmail.com>
2021-04-25 14:00:27 -0400

    cleanup API endpoints

--- javascript/weatherapp/src/app/config/AppConfig.ts
+++ javascript/weatherapp/src/app/config/AppConfig.ts
@@ -1,11 +1,10 @@
 export const containerDiv = <Element>document.querySelector("#container");
 export const apiKey = "92579840e70660f9254cb2c344d28448";
-export const apiLocation = "http://api.openweathermap.org/data/2.5/onecall";
-// ?units=${units}&exclude=minutely,hourly&lat=${lat}&lon=${lon}&appid=${key}
+export const weatherAPI = "http://api.openweathermap.org/data/2.5/onecall";
+export const geocodeAPI = "http://api.openweathermap.org/geo/1.0/direct";
 
 export const debug = false;
 
 export enum enumEventMessages {
   "UPDATE_COMPONENTS",
 }
-
