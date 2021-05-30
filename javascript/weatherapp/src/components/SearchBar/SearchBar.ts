5a3a4b1d9b55d6893489f2766283bf65cb889cce
dis0ps <dis0ps.email@gmail.com>
2021-05-30 12:18:31 -0400

    searchbar styling and behavior

--- javascript/weatherapp/src/components/SearchBar/SearchBar.ts
+++ javascript/weatherapp/src/components/SearchBar/SearchBar.ts
@@ -1,4 +1,5 @@
 import * as app from "../../app/App";
+import styles from "./Searchbar.local.scss";
 import { Component } from "../Prototype/Component";
 
 export class SearchBar extends Component {
@@ -7,29 +8,50 @@ export class SearchBar extends Component {
   constructor(container: Element, pubsub: app.PubSub) {
     super(container);
     this.pubsub = pubsub;
-    this._bindHandler("click", this.handleSearch.bind(this));
+    this._bindHandler("click", this.handleClick.bind(this));
   }
 
-  private handleSearch(e: Event) {
+  private handleClick(e: Event) {
     let targetElement = <HTMLElement>e.target;
     let locationName = <HTMLFormElement>(
       app.containerDiv.querySelector("#location_name")
     );
-    if (targetElement.id == "search") {
-      e.preventDefault();
-      this.pubsub.publish(
-        app.enumEventMessages.SEARCH_LOCATIONS,
-        locationName.value
-      );
+
+    e.preventDefault();
+
+    switch (targetElement.id) {
+      case "search":
+        this.pubsub.publish(
+          app.enumEventMessages.SEARCH_LOCATIONS,
+          locationName.value
+        );
+        locationName.value = ""
+        break;
+      case "metric":
+        this.pubsub.publish(
+          app.enumEventMessages.TOGGLE_UNITS,
+          app.units.METRIC
+        );
+        break;
+      case "imperial":
+        this.pubsub.publish(
+          app.enumEventMessages.TOGGLE_UNITS,
+          app.units.IMPERIAL
+        );
+        break;
     }
   }
 
   protected _updateOutputElement() {
-    //this.outputElement.className = ;
+    this.outputElement.className = styles["searchbar"];
     this.outputElement.innerHTML = `
+   <div class=${styles["header"]}>OpenWeather Search</div>
+   <div>
    <form>
    <input id="location_name" type="text" width=25 />
    <button id="search">Search</button>
+   <button id="metric">C</button>
+   <button id="imperial">F</button>
 
    </form> 
     
