c957be6201bbae1bd51756f04ddd7ade5bfd4828
dis0ps <dis0ps.email@gmail.com>
2021-05-30 12:19:52 -0400

    summary exports added

--- javascript/weatherapp/src/components/Components.ts
+++ javascript/weatherapp/src/components/Components.ts
@@ -1,2 +1,4 @@
 export * from "./SearchBar/SearchBar";
 export * from "./SearchResults/SearchResults";
+export * from "./CurrentWeather/CurrentWeather";
+export * from "./WeatherForecast/WeatherForecast";
